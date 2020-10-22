39ef761ec5851fca864bbab0902e0fb2ca9c708c
Andrey Polonevich <andrey.polonevich@yandex.ru>
2020-10-22 19:00:50 +0300

    Completed the first task

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: +numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const film = prompt('Один из последних просмотренных фильмов?', ''),
+      rating = prompt('На сколько оцените его?', ''),
+      film1 = prompt('Один из последних просмотренных фильмов?', ''),
+      rating1 = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[film] = rating;
+personalMovieDB.movies[film1] = rating1;
+
+console.log(personalMovieDB.movies);
